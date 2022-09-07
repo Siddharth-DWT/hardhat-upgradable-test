89b1567f7de20eceaa51225a91db3f1016591c02
sidarth mishra <smishra9120@gmail.com>
2022-09-07 21:54:01 +0530

    added json address

--- scripts/deploy_all1155.js
+++ scripts/deploy_all1155.js
@@ -1,18 +1,21 @@
 const { ethers} = require("hardhat");
-//import { Contract, ContractFactory } from 'ethers';
 const hardhat = require("hardhat");
-const {MerkleTree} = require('merkletreejs');
-const keccak256 = require('keccak256');
 
-const getMerkleRoot = (addresses)=>{
-    addresses = JSON.parse(addresses)
-     const leaves = addresses.map(x => keccak256(x))
-    const tree = new MerkleTree(leaves, keccak256, { sortPairs: true })
-    const buf2hex = x => '0x' + x.toString('hex')
-    const root = buf2hex(tree.getRoot());
-    console.log(buf2hex(tree.getRoot()))
-    return root;
+const promisify = require('util').promisify;
+const fs = require('fs');
+const writeFile = promisify(fs.writeFile);
+const readFile = promisify(fs.readFile);
+
+const writeAddress = async (key,address) =>{
+    const configJson = await readFile('address.json', 'utf-8');
+    const config = JSON.parse(configJson);
+    Object.assign(config, {[key]:address});
+    await writeFile(
+        'address.json',
+        JSON.stringify(config, null, 2)
+    );
 }
+
 
 async function main() {
     const {BOSS_CARD_URI,GEN1_URI,INGREDIENT_URI,PANCAKE_URI} = process.env;
@@ -21,44 +24,68 @@ async function main() {
     const bosscard = await BossCardERC1155.deploy(BOSS_CARD_URI);
     await bosscard.deployTransaction.wait(10);
     console.log("BosscardERC1155 deployed to:", bosscard.address);
+    writeAddress("BossCardERC1155",bosscard.address)
     console.log('Verifying BosscardERC1155 on Rinkeby...');
-    await hardhat.run('verify:verify', {
-        address: bosscard.address,
-        constructorArguments: [BOSS_CARD_URI]
-    });
+    try{
+        await hardhat.run('verify:verify', {
+            address: bosscard.address,
+            constructorArguments: [BOSS_CARD_URI]
+        });
+    }
+    catch (e){
+        console.log("error",e)
+    }
 
     console.log("Deploying Gen11155...");
     const Gen11155 = await ethers.getContractFactory("Gen11155");
     const gen1 = await Gen11155.deploy(GEN1_URI);
     await gen1.deployTransaction.wait(10);
     console.log("Gen11155 deployed to:", gen1.address);
+    writeAddress("Gen11155",gen1.address)
+
     console.log('Verifying Gen11155 on Rinkeby...');
-    await hardhat.run('verify:verify', {
+    try{
+        await hardhat.run('verify:verify', {
         address: gen1.address,
         constructorArguments: [GEN1_URI]
-    });
+        });
+    }
+    catch (e){
+        console.log("error",e)
+    }
 
     console.log("Deploying IngredientERC1155...");
     const IngredientERC1155 = await ethers.getContractFactory("IngredientERC1155");
     const ingredient = await IngredientERC1155.deploy(INGREDIENT_URI);
     await ingredient.deployTransaction.wait(10);
     console.log("IngredientERC1155 deployed to:", ingredient.address);
+    writeAddress("IngredientERC1155",ingredient.address)
     console.log('Verifying IngredientERC1155 on Rinkeby...');
-    await hardhat.run('verify:verify', {
-        address: ingredient.address,
-        constructorArguments: [INGREDIENT_URI]
-    });
+    try{
+        await hardhat.run('verify:verify', {
+            address: ingredient.address,
+            constructorArguments: [INGREDIENT_URI]
+        });
+    }catch (e){
+        console.log("error",e)
+    }
 
     console.log("Deploying PancakeERC1155...");
     const PancakeERC1155 = await ethers.getContractFactory("PancakeERC1155");
     const pancake = await PancakeERC1155.deploy(PANCAKE_URI);
     await pancake.deployTransaction.wait(10);
     console.log("PancakeERC1155 deployed to:", pancake.address);
+    writeAddress("PancakeERC1155",pancake.address)
     console.log('Verifying PancakeERC1155 on Rinkeby...');
-    await hardhat.run('verify:verify', {
-        address: pancake.address,
-        constructorArguments: [PANCAKE_URI]
-    });
+    try{
+        await hardhat.run('verify:verify', {
+            address: pancake.address,
+            constructorArguments: [PANCAKE_URI]
+        });
+    }
+    catch (e){
+        console.log("error",e)
+    }
 
 }
 
--- scripts/deploy_gen0.js
+++ scripts/deploy_gen0.js
@@ -3,6 +3,10 @@ const { ethers} = require("hardhat");
 const hardhat = require("hardhat");
 const {MerkleTree} = require('merkletreejs');
 const keccak256 = require('keccak256');
+const promisify = require('util').promisify;
+const fs = require('fs');
+const writeFile = promisify(fs.writeFile);
+const readFile = promisify(fs.readFile);
 
 const getMerkleRoot = (addresses)=>{
     addresses = JSON.parse(addresses)
@@ -14,12 +18,25 @@ const getMerkleRoot = (addresses)=>{
     return root;
 }
 
+
+const writeAddress = async (key,address) =>{
+    const configJson = await readFile('address.json', 'utf-8');
+    const config = JSON.parse(configJson);
+    Object.assign(config, {[key]:address});
+    await writeFile(
+        'address.json',
+        JSON.stringify(config, null, 2)
+    );
+}
+
+
 async function main() {
     const [name, symbol, presaleCost, maxSupply, hiddenUri] = process.env.GEN0_PARAMS.split(",")
    // console.log("process.env -ame, symbol, presaleCost, maxSupply, hiddenUri",name, symbol, presaleCost, maxSupply, hiddenUri)
     const merkleRoot = getMerkleRoot(process.env.WHITELIST_ADDRESSES)
     //console.log("merkleRoot",merkleRoot)
     console.log("Deploying...");
+    writeAddress("PowerPlinsGen0ERC721","gen0.address")
     const PowerPlinsGen0 = await ethers.getContractFactory("PowerPlinsGen0ERC721");
     const gen0 = await PowerPlinsGen0.deploy(
         name,
@@ -29,22 +46,28 @@ async function main() {
         hiddenUri,
         merkleRoot
     );
+
     await gen0.deployTransaction.wait(10);
     console.log("PowerPlinsGen0ERC721 deployed to:", gen0.address);
+    writeAddress("PowerPlinsGen0ERC721",gen0.address)
 
     /**
      * Verify Contracts
      */
     console.log('Verifying PowerPlinsGen0ERC721 on Rinkeby...');
-    await hardhat.run('verify:verify', {
-        address: gen0.address,
-        constructorArguments: [ name,
-            symbol,
-            presaleCost,
-            maxSupply,
-            hiddenUri,
-            merkleRoot]
-    });
+    try{
+        await hardhat.run('verify:verify', {
+            address: gen0.address,
+            constructorArguments: [ name,
+                symbol,
+                presaleCost,
+                maxSupply,
+                hiddenUri,
+                merkleRoot]
+        });
+    }catch (e){
+        console.log("error",e)
+    }
 }
 
 main();

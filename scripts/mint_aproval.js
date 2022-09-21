e51faaf0dc3f49cec6141ee7138f23596d31203f
sidarth mishra <smishra9120@gmail.com>
2022-09-22 03:52:55 +0530

    added mint approval

--- scripts/mint_aproval.js
+++ scripts/mint_aproval.js
@@ -1,25 +1,27 @@
-const {CONTRACT_NAME_MAP, verifyProxyContract} = require("../utils/common")
+const {CONTRACT_NAME_MAP, verifyProxyContract, approveContract} = require("../utils/common")
 const address= require("../address.json")
 const {ethers} = require("hardhat");
 async function main(){
-    const {IngredientsERC11155,BossCardERC1155,PancakeNftERC11155, Gen1ERC1155, ErrandGen0,ErrandGen1, Cook} = address
+    const {PowerPlinsGen0ERC721,IngredientsERC11155,BossCardERC1155,PancakeNftERC11155, Gen1ERC1155, SignatureChecker, ErrandGen0,ErrandGen1, Cook, Shrine} = address
 
-    const Ingredient = await ethers.getContractFactory(CONTRACT_NAME_MAP.IngredientsERC11155);
-    const IngredientContract = Ingredient.attach(address.IngredientsERC11155);
+    //ERRAND
+    await approveContract(CONTRACT_NAME_MAP.IngredientsERC11155, IngredientsERC11155, ErrandGen0, true,"ErrandGen1")
+    await approveContract(CONTRACT_NAME_MAP.IngredientsERC11155, IngredientsERC11155, ErrandGen1, true,"ErrandGen0")
 
-    const BossCard = await ethers.getContractFactory(CONTRACT_NAME_MAP.BossCardERC1155);
-    const BossContract = BossCard.attach(address.BossCardERC1155);
+    //COOK
+    await approveContract(CONTRACT_NAME_MAP.PancakeNftERC11155,PancakeNftERC11155,Cook, true,'Cook')
 
-    const Pancake = await ethers.getContractFactory(CONTRACT_NAME_MAP.PancakeNftERC11155);
-    const PancakeContract = Pancake.attach(address.PancakeNftERC11155);
+    //SHRINE
+    await approveContract(CONTRACT_NAME_MAP.PancakeNftERC11155,PancakeNftERC11155,Shrine, true,'Shrine')
+    await approveContract(CONTRACT_NAME_MAP.Gen1ERC1155,Gen1ERC1155,Shrine, true,'Shrine')
 
-    const Gen1 = await ethers.getContractFactory(CONTRACT_NAME_MAP.Gen1ERC1155);
-    const Gen1Contract = Gen1.attach(address.Gen1ERC1155);
-    await IngredientContract.setMintApprovalForAll(ErrandGen0, true)
-    await IngredientContract.setMintApprovalForAll(ErrandGen1, true)
-    await PancakeContract.setMintApprovalForAll(Cook,true)
-    //await PancakeContract.setMintApprovalForAll(Cook,true)
-    //await PancakeContract.setMintApprovalForAll(Cook,true)
+    const ErrandGen0Contract = await ethers.getContractFactory(CONTRACT_NAME_MAP.SignatureChecker);
+    const DeployedSignatureChecker = ErrandGen0Contract.attach(SignatureChecker);
+
+    console.log("Updating ValidatorAddress....")
+    await DeployedSignatureChecker.setValidatorAddress(process.env.SIGNATURE_VALIDATOR)
+    console.log(`address ${process.env.SIGNATURE_VALIDATOR} updated`)
+
 
 
 }
